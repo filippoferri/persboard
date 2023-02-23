d7b85a7fce27fa5119f4586b3e8834d60292542b
filippoferri <hello@filippoferri.it>
2023-02-23 22:29:39 +0100

    changed register title

--- src/sections/auth/Register.js
+++ src/sections/auth/Register.js
@@ -13,7 +13,7 @@ import AuthRegisterForm from './AuthRegisterForm';
 
 export default function Register() {
   return (
-    <LoginLayout title="Manage the job more effectively with Minimal">
+    <LoginLayout title="Discover a trusted advisor to turn to at every major decision.">
       <Stack spacing={2} sx={{ mb: 5, position: 'relative' }}>
         <Typography variant="h4">Get started absolutely free.</Typography>
 
