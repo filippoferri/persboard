a06d9bea0f73070a81dd133ecab7e5c9b6a66db8
filippoferri <hello@filippoferri.it>
2023-03-05 11:01:16 +0100

    form fixed

--- src/sections/@dashboard/general/welcome/welcomeboard/boardfromscratch.js
+++ src/sections/@dashboard/general/welcome/welcomeboard/boardfromscratch.js
@@ -18,56 +18,144 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
     width: 200,
 }));
 
-const Item = ({ number }) => (
-    <Card sx={{ p: 2, mb: 1 }}>
-        <FormProvider methods={useForm()}>
-            <Stack direction="row" spacing={2} spacing={1}>
-                <StyledPaper
-                    sx={{
-                        display: 'flex',
-                        backgroundColor: '#D1E9FC',
-                        borderRadius: 1,
-                        width: '50px',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }} >
-                        <Typography variant="h5">{number}</Typography>
-                </StyledPaper>
+const Item = ({ number }) => {
 
-                <StyledPaper sx={{ width: 700 }}>
-                    <RHFTextField name={`fullName${number}`} label="John Doe" helperText="Fake or Real Full Name (also famous)" />
-                </StyledPaper>
-                <StyledPaper sx={{ flexGrow: 1 }}>
-                    <RHFTextField select name="role" helperText="Role Director" defaultValue="Mentor">
-                        {roles.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                        </MenuItem>
-                        ))}
-                    </RHFTextField>
-                </StyledPaper>
-                <StyledPaper sx={{ flexGrow: 1 }}>
-                    <RHFTextField select name="area" helperText="Area Expertise" defaultValue="Advocacy">
-                        {areas.map((option, index) => (
+    const [showAdvanced, setShowAdvanced] = useState(false);
+
+    const [inputFields, setInputFields] = useState([
+      {
+        fullName: '', 
+        role: 'Mentor',
+        area: 'Advocacy',
+        quality: 'Vision',
+      }
+    ])
+
+    const { register, handleSubmit, watch } = useForm({
+      resolver: yupResolver(
+        Yup.object().shape({
+          fullName: Yup.string().when(['role', 'area', 'quality'], {
+            is: (role, area, quality) => role || area || quality,
+            then: Yup.string().required('Name is required'),
+          }),
+        })
+      ),
+    });
+
+    const handleAdvancedSubmit = (data) => {
+      onSubmit(data);
+      setShowAdvanced(false);
+    };
+
+    const handleFormChange = (index, event) => {
+      let data = [...inputFields];
+      data[index][event.target.name] = event.target.value;
+      setInputFields(data);
+    }
+
+    return (
+      <Card sx={{ p: 2, mb: 1 }}>
+          <FormProvider methods={useForm()}>
+              {inputFields.map((input, index) => {
+                return (
+                <Stack key={index} direction="row" spacing={2} spacing={1}>
+                    <StyledPaper
+                        sx={{
+                            display: 'flex',
+                            backgroundColor: '#D1E9FC',
+                            borderRadius: 1,
+                            width: '50px',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }} >
+                            <Typography variant="h5">{number}</Typography>
+                    </StyledPaper>
+
+                    <StyledPaper sx={{ flexGrow: 6 }}>
+                        <RHFTextField 
+                          name={'fullName'} 
+                          label="John Doe" 
+                          helperText="Fake or Real Full Name (also famous)" 
+                          value={input.fullName}
+                          onChange={event => handleFormChange(index, event)}
+                        />
+                    </StyledPaper>
+
+                {showAdvanced ? (
+                <>
+
+                  <StyledPaper sx={{ flexGrow: 1 }}>
+                    <RHFTextField
+                      select
+                      name="role"
+                      helperText="Role Director"
+                      value={input.role}
+                      onChange={event => handleFormChange(index, event)}
+                    >
+                      {roles.map((option, index) => (
                         <MenuItem key={index} value={option.value}>
-                            {option.label}
+                          {option.label}
                         </MenuItem>
-                        ))}
+                      ))}
                     </RHFTextField>
-                </StyledPaper>
-                <StyledPaper sx={{ flexGrow: 1 }}>
-                    <RHFTextField select name="area" helperText="Key Quality" defaultValue="Vision">
-                        {qualities.map((option, index) => (
+                  </StyledPaper>
+
+                  <StyledPaper sx={{ flexGrow: 1 }}>
+                    <RHFTextField
+                      select
+                      name="area"
+                      helperText="Area Expertise"
+                      value={input.area}
+                      onChange={event => handleFormChange(index, event)}
+                    >
+                      {areas.map((option, index) => (
                         <MenuItem key={index} value={option.value}>
-                            {option.label}
+                          {option.label}
                         </MenuItem>
-                        ))}
+                      ))}
                     </RHFTextField>
+                  </StyledPaper>
+                  
+                  <StyledPaper sx={{ flexGrow: 1 }}>
+                    <RHFTextField
+                      select
+                      name="quality"
+                      helperText="Key Quality"
+                      value={input.quality}
+                      onChange={event => handleFormChange(index, event)}
+                    >
+                      {qualities.map((option, index) => (
+                        <MenuItem key={index} value={option.value}>
+                          {option.label}
+                        </MenuItem>
+                      ))}
+                    </RHFTextField>
+                  </StyledPaper>
+                </>
+              ) : (
+                <StyledPaper
+                  sx={{ alignItems: 'center', justifyContent: 'center' }}
+                  onClick={() => setShowAdvanced(true)}
+                >
+                  <Typography sx={{
+                    variant: "caption",
+                    cursor: "pointer",
+                    display: "flex",
+                    height: "56px",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#3f51b5",
+                  }}>Advanced AI</Typography>
                 </StyledPaper>
-            </Stack>
-        </FormProvider>
-    </Card>
-);
+              )}
+
+                </Stack>
+                )
+              })}
+          </FormProvider>
+      </Card>
+    );
+};
 
 const roles = [
 {
@@ -170,13 +258,19 @@ const qualities = [
     },
 ];
 
-const BoardFromScratch = ({onClick}) => {
+const BoardFromScratch = ({onNextStep, dataFromPrevStep}) => {
     const UpdateBoardSchema = Yup.object().shape({
-        fullName: Yup.string().required('Name is required'),
+        fullName1: Yup.string().required('Name is required'),
+        fullName2: Yup.string().required('Name is required'),
+        fullName3: Yup.string().required('Name is required'),
     });
 
     const defaultValues = {
-        fullName: '',
+        fullName1: '',
+        fullName2: '',
+        fullName3: '',
+        fullName4: '',
+        fullName5: '',
     };
 
     const methods = useForm({
@@ -184,19 +278,34 @@ const BoardFromScratch = ({onClick}) => {
         defaultValues,
     });
 
+    const [boardData, setBoardData] = useState([]);
+
     const MAX_ITEMS = 5;
     const [count, setCount] = useState(3);
 
     const handleAdd = () => {
-        if (count < MAX_ITEMS) {
-            setCount((prevCount) => prevCount + 1);
-        }
+      if (count < MAX_ITEMS) {
+        setCount((prevCount) => prevCount + 1);
+      }
     };
 
     const items = [];
     for (let i = 1; i <= count; i++) {
         items.push(<Item key={i} number={i} />);
     }
+
+    const handleClick = () => {
+      setBoardData((dataFromPrevStep) => [
+        ...dataFromPrevStep,
+        {
+          fullName: '',
+          role: 'Mentor',
+          area: 'Advocacy',
+          quality: 'Leadership',
+        },
+      ]);
+      onNextStep();
+    };
 
     return (
     <>
@@ -209,7 +318,7 @@ const BoardFromScratch = ({onClick}) => {
             }}
         >
             <Box sx={{ flexGrow: 1 }}>
-                Select from 3 to 5 people who will be on your board.
+                Add from 3 to 5 people who will be on your board.
             </Box>
             <Box sx={{ flexShrink: 0 }}></Box>
         </Stack>
@@ -227,7 +336,7 @@ const BoardFromScratch = ({onClick}) => {
         >
             <Box sx={{ flexGrow: 1 }}></Box>
             <Box sx={{ flexShrink: 0 }}>
-                <Button variant="contained" size="large" onClick={onClick} >
+                <Button variant="contained" size="large" onClick={handleClick} >
                     Ask Your Board
                 </Button>
             </Box>
